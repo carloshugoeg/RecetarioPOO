1e19cfdaa67e940231ba19cd111f2e5e958d5805
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-31 09:33:34 -0600

    Se hacen avances en la logica de ingredientes y su relacion con las recetas

---
+++ RecetarioPOO/Ingredient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetarioPOO
+{
+    internal class Ingredient
+    {
+        public Ingredient(string ingredientName, string ingredientAmount)
+        {
+            this.ingredientName = ingredientName;
+            this.ingredientAmount = ingredientAmount;
+        }
+
+        private string ingredientName {  get; set; }
+        private string ingredientAmount { get; set; }
+
+        public void AddIngredient()
+        {
+            List<Ingredient> ingredientList = new List<Ingredient>();
+            do
+            {
+                Console.WriteLine("---NEW INGREDIENT---");
+                Console.WriteLine("\nIngredient Name: ");
+                ingredientName = Console.ReadLine();
+                Console.WriteLine("Amount: ");
+                ingredientName = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nIngredient added succesfully!!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.WriteLine("\nWant to add anonther ingredient? y/n");
+                string option = Console.ReadLine().ToLower().Trim();
+                if (option == "n") break;
+            } while (true);
+
+        }
+    }
+}
---
+++ RecetarioPOO/Recipes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetarioPOO
+{
+    internal class Recipes
+    {
+        public Recipes(string name, string description, List<Ingredient> ingredientList, string procedure)
+        {
+            RecipeName = name;
+            RecipeDescription = description;
+            IngredientList = ingredientList;
+            RecipeProcedure = procedure;
+        }
+
+        private string RecipeName { get; set; }
+        private string RecipeDescription { get; set; }
+        private List<Ingredient> IngredientList { get; set; }
+        private string RecipeProcedure {  get; set; }
+
+        public void NewRecipe()
+        {
+            Console.Clear();
+            Console.WriteLine("-----NEW RECIPE----");
+            Console.WriteLine("\nRecipe name: ");
+            Console.WriteLine("Please Proceed to add your igredients\nPress ENTER to continue");
+            IngredientList
+            Console.ReadLine();
+            
+        }
+    }
+}
