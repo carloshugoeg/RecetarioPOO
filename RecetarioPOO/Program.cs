678cc5dde0944479a455f4aace4368922c0464ac
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-07-31 09:07:42 -0600

    Agregar archivos de proyecto.

---
+++ RecetarioPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RecetarioPOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------RECIPES-------");
+            Console.WriteLine("\n     1. NEW RECIPE");
+            Console.WriteLine("\n     2. SEARCH RECIPE");
+            Console.WriteLine("\n     3. SHOW ALL");
+            string option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+
+                    break;
+            }
+    }
+}
